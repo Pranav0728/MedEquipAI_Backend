d55d283de01bd5ecc0c242d6501fa30bd8776726
Pranav <pranavmolawade123@gmail.com>
2026-08-27 11:15:13 +0530

    app.js

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ const app = express();
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:3000',
-    'https://med-equip-ai-frontend.vercel.app/',
+    'https://medequipai.vercel.app',
 ];
 
 app.use(
